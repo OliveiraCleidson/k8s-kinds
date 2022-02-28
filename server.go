fe8203f56397d14c8b279e7958a9421091ab062a
Cleidson Oliveira <dev.cleidson@gmail.com>
2022-02-28 01:48:05 -0300

    first commit

---
+++ server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", Hello)
+	http.HandleFunc("/configmap", ConfigMap)
+	http.ListenAndServe(":8000", nil)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("myfamily/family.txt")
+	if err != nil {
+		log.Fatalf("Error reading file: ", err)
+	}
+
+	fmt.Fprintf(w, "My family %s.", string(data))
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+
+	fmt.Fprintf(w, "Hello, I'm %s. I'm %s", name, age)
+}
